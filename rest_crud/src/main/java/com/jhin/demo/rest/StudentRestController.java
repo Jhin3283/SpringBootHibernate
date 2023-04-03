52ecc0a4e3f8ef22a7be7cd3e441ccc382496db7
jhin3283 <hj3283@gmail.com>
2023-04-03 11:22:46 +0900

    ADD: REST CRUD

---
+++ rest_crud/src/main/java/com/jhin/demo/rest/StudentRestController.java
@@ -0,0 +1,42 @@
+package com.jhin.demo.rest;
+
+import com.jhin.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> theStudents;
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Jhin","Jeong"));
+        theStudents.add(new Student("Mario","Rossi"));
+        theStudents.add(new Student("Marry","Smith"));
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        if((studentId >= theStudents.size()) || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> hadeleException(StudentErrorResponse exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
