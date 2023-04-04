356ebee8ba7b0278a3d392fac2f5b6605d9c9511
jhin3283 <hj3283@gmail.com>
2023-04-04 12:53:27 +0900

    ADD: Rest API Using JPA

---
+++ crud_jpa/src/main/java/com/jhin/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.jhin.springboot.cruddemo.dao;
+
+import com.jhin.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
